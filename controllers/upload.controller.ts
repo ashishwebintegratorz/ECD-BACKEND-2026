97e0b9db65dfe7de56c84e15ccca210775afee3f
jaytiwari2004 <jaytiwari55798@gmail.com>
2026-05-20 18:55:18 +0530

    debug: add console logs for req.file and req.files in uploadImage

--- controllers/upload.controller.ts
+++ controllers/upload.controller.ts
@@ -3,6 +3,13 @@ import { imagekit } from "../config/imagekit.js";
 import { BadRequestException } from "../utils/appError.js";
 
 export const uploadImage = async (req: Request, res: Response) => {
+    // Debug incoming files payload
+    console.log("DEBUG [uploadImage]: req.file =", req.file);
+    console.log("DEBUG [uploadImage]: req.files =", (req as any).files);
+    if ((req as any).files && (req as any).files[0]) {
+        console.log("DEBUG [uploadImage]: req.files[0] =", (req as any).files[0]);
+    }
+
     if (!req.file) {
         throw new BadRequestException("No file provided");
     }
